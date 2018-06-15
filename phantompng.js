032622c1a855bca4eaea6e10cbe79e363af2562e
Graziela <graziieng@gmail.com>
2018-06-15 05:14:03 +0000

    criando outro screenshot da página com phantom, agora em formato png

---
+++ phantompng.js
@@ -0,0 +1,6 @@
+//arquivo js, com função parar dar screenshot (png) da página //
+var page = require('webpage').create();
+page.open('https://trabalho-api-grazifm.c9users.io/index.html', function() {
+  page.render('musicalizando.png');
+  phantom.exit();
+});
